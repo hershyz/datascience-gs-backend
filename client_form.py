b1f4a03817280488ffedd41ecb1b604901f331b8
Hrishikesh Bagalkote <hbagalkote@gmail.com>
2022-08-30 11:55:40 -0400

    init client form

---
+++ client_form.py
@@ -0,0 +1,6 @@
+valid_teachers = ['Julia Rachkovskiy', 'Laura Gray', '_____']
+
+student_id = input('Enter your student ID: ')
+fname = input('Enter your first name: ')
+lname = input('Enter your last name: ')
+intervention_teacher = input('Intervention teacher (first and last name): ')
