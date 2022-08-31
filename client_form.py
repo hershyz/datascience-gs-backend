f75bb59aca96da26e88f8811814f27fb023f9f04
Hrishikesh Bagalkote <hbagalkote@gmail.com>
2022-08-31 11:17:10 -0400

    init form

--- client_form.py
+++ client_form.py
@@ -1,6 +1,17 @@
-valid_teachers = ['Julia Rachkovskiy', 'Laura Gray', '_____']
+from ast import Interactive
 
-student_id = input('Enter your student ID: ')
-fname = input('Enter your first name: ')
-lname = input('Enter your last name: ')
-intervention_teacher = input('Intervention teacher (first and last name): ')
+
+valid_teacher_emails = ['julia.rachkovskiy@gcpsk12.org', 'elfi.funk@gcpsk12.org', 'jeff.burmester@gcpsk12.org', 'bradley.kastner@gcpsk12.org', 'hbagalkote@gmail.com'] # my email for test
+
+def main():
+    
+    student_id = input('Enter your student ID: ')
+    fname = input('Enter your first name: ')
+    lname = input('Enter your last name: ')
+    intervention_teacher = input('Intervention teacher email: ').lower()
+    
+    if intervention_teacher not in valid_teacher_emails:
+        print('Invalid intervention teacher email, valid ones are: ' + str(valid_teacher_emails))
+        main()
+    
+    # send post request
