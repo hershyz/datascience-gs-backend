78f6c868763e4a821c7585060f021e2854fdd5b3
Hrishikesh Bagalkote <hbagalkote@gmail.com>
2022-09-01 11:31:20 -0400

    db csv

--- server.py
+++ server.py
@@ -8,11 +8,22 @@ def default():
 
 @app.route('/form_filled', methods=['POST'])
 def form_filled():
+    
     data = request.json
-    print(data['student_id'])
-    print(data['fname'])
-    print(data['lname'])
-    print(data['intervention_teacher'])
+    
+    # print(data['student_id'])
+    # print(data['fname'])
+    # print(data['lname'])
+    # print(data['intervention_teacher'])
+
+    student_id = str(data['student_id'])
+    fname = str(data['fname'])
+    lname = str(data['lname'])
+    intervention_teacher = str(data['intervention_teacher'])
+    f = open('db.csv', 'a')
+    f.write(student_id + ',' + fname + ',' + lname + ',' + intervention_teacher + '\n')
+    f.close()
+    
     return 'res ok'
 
 if __name__ == "__main__":
